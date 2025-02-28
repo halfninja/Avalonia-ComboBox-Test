86102f9bbd5ebacd440d690546b52b4156ce7411
Nick Howes <nick@nickhowes.co.uk>
2025-02-28 22:22:47 +0000

    Initial commit.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,22 @@
-﻿namespace ComboBoxTest.ViewModels;
+﻿using System.Collections.ObjectModel;
+using ComboBoxTest.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ComboBoxTest.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    [ObservableProperty]
+    private ObservableCollection<MyListItem> _items = [
+        new() { Title = "Red", Count = 1 },
+        new() { Title = "Green", Count = 1 },
+        new() { Title = "Blue", Count = 1 },
+    ];
+
+    [RelayCommand]
+    private void AddItem()
+    {
+        Items.Add(new MyListItem { Title = "New Item", Count = 1 });
+    }
 }
--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,15 +1,5 @@
-﻿using System.Collections.ObjectModel;
-using ComboBoxTest.Models;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ComboBoxTest.ViewModels;
 
-public partial class ViewModelBase : ObservableObject
-{
-    [ObservableProperty]
-    private ObservableCollection<MyListItem> _items = [
-        new() { Title = "Red", Count = 1 },
-        new() { Title = "Green", Count = 1 },
-        new() { Title = "Blue", Count = 1 },
-    ];
-}
+public partial class ViewModelBase : ObservableObject;
