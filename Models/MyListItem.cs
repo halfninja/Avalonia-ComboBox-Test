a2974692d3768d02fe50efaf6ff995ccfcd095cb
Nick Howes <nick@nickhowes.co.uk>
2025-02-28 22:22:47 +0000

    Initial commit.

---
+++ Models/MyListItem.cs
@@ -0,0 +1,7 @@
+namespace ComboBoxTest.Models;
+
+public class MyListItem
+{
+    public required string Title { get; set; }
+    public int Count { get; set; } = 0;
+};
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using ComboBoxTest.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ComboBoxTest.ViewModels;
+
+public partial class ViewModelBase : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<MyListItem> _items = [
+        new() { Title = "Red", Count = 1 },
+        new() { Title = "Green", Count = 1 },
+        new() { Title = "Blue", Count = 1 },
+    ];
+}
